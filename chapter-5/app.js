31b9c3c3125ad7b8b46c1fef1d0c3533476b3dc6
Syedhaiderali <shaiderali550@gmail.com>
2024-03-02 07:02:13 +0500

    push

---
+++ chapter-5/app.js
@@ -0,0 +1,106 @@
+var english =80
+var urdu = 90
+
+ var math = english + urdu;
+
+console.log("sum of 80 and 90 in " + math);
+
+var num1 = 200
+var num2 = 100
+var subtr = num1- num2;
+console.log("sum of 200 and 100 in " + subtr);
+
+
+var sub = 7
+var sub2 = 6
+var pakistan = sub * sub2
+console.log("sum of 7 and 6 in " + pakistan);
+
+document.write("sum of 80 and 90 in " + math)
+document.write("<br><br>")
+
+document. write("sum of 200 and 100 in " + subtr);
+document.write("<br><br>")
+
+document. write("sum of 7 and 6 in " + pakistan); 
+document.write("<br><br>")
+
+ 
+var pub1 = 4
+var pub2 = 2
+ var pub = pub1  % pub2
+console.log("sum of 4 and 2 in " + pub );
+ document.write("sum of 4 and 2 in " + pub);
+ document.write("<br><br>")
+
+ 
+var pub1 = 550
+var pub2 = 3
+ var eng = pub1  % pub2
+console.log("sum of 550 and 3 in " + eng );
+ document.write("sum of 550 and 3in " + eng);
+  document.write("<br><br>")
+    
+
+  ///pakistan42
+ ////subtr100
+  //// math 170
+  ///
+
+  
+
+  var haider1 = 300
+  var haider2 = 4
+ var haider = haider1 / haider2
+  console.log( "sum of 300 and 4 in " +haider)
+  document.write("um of 300 and 4 in " +haider)
+  document.write("<br><br>")
+
+
+   var india = pakistan / subtr + math
+   console.log(" sum of mult and div in addition " + india)
+   document.write(india)
+   document.write("<br></br>");
+
+
+   var bom = pakistan + subtr * eng
+   
+   console.log(bom)
+   document.write(bom)
+    document.write("<br> <br>")
+
+    var phum1 = 300
+    var phum2 = phum1++
+     console.log(  phum1)
+     document.write("increment " + phum1)
+     document.write("<br>")
+     console.log(phum2)
+     document.write("increment " + phum2)
+       
+     
+
+
+    var totalMarks = 500; // Change this value to the total marks
+    var marksObtained = 420; // Change this value to the marks obtained by the student
+    
+    // Calculate the percentage
+    var percentage = (marksObtained / totalMarks) * 100;
+
+    // Display the result in the browser
+    document.write("<h2>Percentage Calculator</h2>");
+    document.write("<p>Total Marks: " + totalMarks + "</p>");
+    document.write("<p>Marks Obtained: " + marksObtained + "</p>");
+    document.write("<p>Percentage: " + percentage.toFixed(2) + "%</p>");
+      
+
+
+
+    
+    //Example usage:
+    var age = 25; // Age in years
+    var amountPerDay = 2; // Average amount consumed per day
+    var maxAge = 80; // Estimated maximum age
+    
+    var lifetimeSupply = calculateLifetimeSupply(age, amountPerDay, maxAge);
+    console.log("You will need " + lifetimeSupply + " units of your favorite snack to last you until the age of " + maxAge + ".");
+    
