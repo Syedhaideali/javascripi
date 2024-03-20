aae2cf134ea570164077d8124be21515767e79f7
Syedhaiderali <shaiderali550@gmail.com>
2024-03-20 19:11:21 +0500

    new
    new

---
+++ new chapter/app.js
@@ -0,0 +1,36 @@
+var type = ['haider' , 'umar' , 'zain ali']
+console.log(type)
+type.push('mustafa' , 'khald')
+console.log(type)
+
+
+
+  //remove alements at the end of aaray//
+
+  var type = ['haider' , 'umar' , 'zain ali']
+  console.log(type)
+  type.pop()
+  console.log(type)
+  
+  
+
+
+  type.unshift('new type')
+  console.log(type)
+  
+
+
+  type.shift()
+  console.log(type)
+
+
+
+
+
+
+
+
+
+
+
+  
